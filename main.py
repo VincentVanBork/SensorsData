919f5ba2e13cf5733cc2c2884432409b7d7107fc
Michał Darowny <michal.darowny@gmail.com>
2021-01-18 17:01:54 +0100

    naprawione obroty

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from gyro_sensor import gyro1
 sns.set_theme()
 data = import_data("acce21.csv")
 # print(data.head())
-filter_acceleration(data, div_freq=20)
+# filter_acceleration(data, div_freq=20)
 # x_wave=wavelets(data = data["X"], wavelet = 'haar', uselevels = 3, mode = 'zero')
 # y_wave=wavelets(data = data["Y"], wavelet = 'haar', uselevels = 3, mode = 'zero')
 # z_wave=wavelets(data = data["Z"], wavelet = 'haar', uselevels = 3, mode = 'zero')
@@ -27,24 +27,26 @@ filter_acceleration(data, div_freq=20)
 # sns.lineplot(y=data["X"], x=range(len(data["X"])))
 # sns.lineplot(y=x_wave,  x=range(len(x_wave)))
 
-acce_vectors = data[["X_filter","Y_filter","Z_filter"]].to_numpy()
+acce_vectors = data[["X","Y","Z"]].to_numpy()
 rotation_vectors = gyro1[["X","Y","Z","ANGLE"]].to_numpy()
 rotation = R.from_quat(rotation_vectors)
 rotated_vectors = rotation.apply(acce_vectors)
 rotated_acce = pd.DataFrame(rotated_vectors, columns=["X", "Y", "Z"])
 
 plt.figure()
-sns.lineplot(y=data["Y_filter"], x=data["TIME"], label="Y_NORMAL")
+sns.lineplot(y=data["Y"], x=data["TIME"], label="Y_NORMAL")
 sns.lineplot(y=rotated_acce["Y"], x=data["TIME"], label="Y_ROTATED")
 
+data["X"] = rotated_acce["X"]
+data["Y"] = rotated_acce["Y"]
+data["Z"] = rotated_acce["Z"]
 
-data["X_filter"] = rotated_acce["X"]
-data["Y_filter"] = rotated_acce["Y"]
-data["Z_filter"] = rotated_acce["Z"]
-
+filter_acceleration(data, div_freq=20)
 
 
 calculate_velocity(data)
+
+
 
 # data["X_velocity"] = regres(data["X_velocity"], data["TIME"], 100, len(data)-1)
 # data["Y_velocity"] = regres(data["Y_velocity"], data["TIME"], 100, len(data)-1)
@@ -61,9 +63,12 @@ calculate_velocity(data)
 # set_equal_velocity(data, y0, yf, "Y_velocity")
 # set_equal_velocity(data, z0, zf, "Z_velocity")
 
-data["X_velocity"]= signal.detrend(data["X_velocity"], type='constant')
-data["Y_velocity"] = signal.detrend(data["Y_velocity"], type='constant')
-data["Z_velocity"] = signal.detrend(data["Z_velocity"], type='constant')
+data = data.dropna()
+
+# data["X_velocity"]= signal.detrend(data["X_velocity"])
+data["Y_velocity"] = signal.detrend(data["Y_velocity"])
+# data["Z_velocity"] = signal.detrend(data["Z_velocity"])
+
 
 calculate_position(data)
 
